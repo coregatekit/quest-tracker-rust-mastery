43eeac2e90e76671cb53decb5863ba1b685a6ee6
Joe Kim <joesm.genie@gmail.com>
2025-08-10 14:36:28 +0700

    Fix typo in environment variable name for server timeout

--- src/config/config_loader.rs
+++ src/config/config_loader.rs
@@ -14,8 +14,8 @@ pub fn load() -> Result<DotEnvyConfig> {
         body_limit: std::env::var("SERVER_BODY_LIMIT")
             .expect("SERVER_BODY_LIMIT is invalid")
             .parse()?,
-        time_out: std::env::var("SERVER_TIME_OUT")
-            .expect("SERVER_TIME_OUT is invalid")
+        time_out: std::env::var("SERVER_TIMEOUT")
+            .expect("SERVER_TIMEOUT is invalid")
             .parse()?,
     };
 
