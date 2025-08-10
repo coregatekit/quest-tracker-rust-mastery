e08948aa053d4ffabf9c3a1d83be582da4d6a458
Joe Kim <joesm.genie@gmail.com>
2025-08-10 14:36:01 +0700

    Refactor main function to load environment variables and initialize logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use quest_tracker_rust_mastery::config::config_loader;
+use tracing::{error, info};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().with_max_level(tracing::Level::DEBUG).init();
+
+    let dotenvy_env = match config_loader::load() {
+        Ok(env) => env,
+        Err(e) => {
+            error!("Failed to load ENV: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    info!("ENV has been loaded successfully: {:?}", dotenvy_env);
 }
