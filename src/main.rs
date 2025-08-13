2b3e9b1734829130c9eeb0c02c501bcedf293bb8
Joe Kim <joesm.genie@gmail.com>
2025-08-13 10:51:10 +0700

    Test connect database

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use quest_tracker_rust_mastery::config::config_loader;
+use quest_tracker_rust_mastery::{
+    config::config_loader, infrastructure::postgres::postgres_connection,
+};
 use tracing::{error, info};
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt().with_max_level(tracing::Level::DEBUG).init();
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
 
     let dotenvy_env = match config_loader::load() {
         Ok(env) => env,
@@ -14,4 +18,14 @@ async fn main() {
     };
 
     info!("ENV has been loaded successfully: {:?}", dotenvy_env);
+
+    let postgres_pool = match postgres_connection::establish_connection(&dotenvy_env.database.url) {
+        Ok(pool) => pool,
+        Err(e) => {
+            error!("Failed to establish PostgreSQL connection: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    info!("PostgreSQL connection established successfully");
 }
