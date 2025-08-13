7e8b8412f44075407ba60252fecec9a5d3d45b31
Joe Kim <joesm.genie@gmail.com>
2025-08-13 16:59:35 +0700

    Add AdventurersRepository and GuildCommandersRepository traits with async methods for registration and retrieval

---
+++ src/domain/repositories/adventurers.rs
@@ -0,0 +1,15 @@
+use anyhow::Result;
+use axum::async_trait;
+use mockall::automock;
+
+use crate::domain::{
+    entities::adventurers::AdventurerEntity,
+    value_objects::adventurer_model::RegisterAdventurerModel,
+};
+
+#[async_trait]
+#[automock]
+pub trait AdventurersRepository {
+    async fn register(&self, register_adventurer_mode: RegisterAdventurerModel) -> Result<i32>;
+    async fn find_by_username(&self, username: String) -> Result<AdventurerEntity>;
+}
